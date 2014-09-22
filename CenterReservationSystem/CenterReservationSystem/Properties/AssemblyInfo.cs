2b8c2721efabd05b3cf80c8a591044707f7c4fb3
mohamed-khalil88 <eng.mohamed_khalil88@yahoo.com>
2014-09-22 22:33:12 +0300

    Changed nams of projects

--- CenterReservationSystem/CenterReservationSystem/Properties/AssemblyInfo.cs
+++ CenterReservationSystem/CenterReservationSystem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CenterReservationSystem")]
+[assembly: AssemblyTitle("CenterReservation.INT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CenterReservationSystem")]
+[assembly: AssemblyProduct("CenterReservation.INT")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- CenterReservationSystem/DAL/Program.cs
+++ CenterReservationSystem/DAL/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DAL
+namespace CenterReservation.DL
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace DAL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
